384c00e4f0dc83f4bde92185820b12c5bd304595
mosojnicki <mosojnic@gmail.com>
2022-01-04 23:53:35 +0100

    isporuka-v1

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,109 @@
+﻿using OsigDrustvo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+
+namespace OsigDrustvo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //public SqlConnection con;
+        string connString = ConfigurationManager.ConnectionStrings["myConnStr"].ToString();
+
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<Partner> partners = new List<Partner>();
+            using (IDbConnection db = new SqlConnection(connString))
+            {
+                partners = db.Query<Partner>("SELECT * FROM[OsigDrustvo].[dbo].[Partners] ORDER BY CreatedAtUtc DESC, PartnerId DESC").ToList();
+            }
+            return View(partners);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        private bool IsExternalCodeExist(Partner partner)
+        {
+            bool exist;
+
+            try
+            {
+                using (IDbConnection db = new SqlConnection(connString))
+                {
+                    string upit = "SELECT COUNT (ExternalCode) ExternalCode FROM [dbo].[Partners] WHERE ExternalCode=@ExternalCode";
+                    exist = db.ExecuteScalar<int>(upit, new { partner.ExternalCode }) > 0;
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return exist;
+
+        }
+        [HttpPost]
+        public ActionResult Create(Partner partner)
+        {
+            if (IsExternalCodeExist(partner))
+            {
+                ModelState.AddModelError("ExternalCode", "External code already exist");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    using (IDbConnection db = new SqlConnection(connString))
+                    {
+                        string sql = @"
+INSERT INTO [dbo].[Partners]
+(FirstName, LastName, Address, PartnerNumber, CroatianPIN, PartnerTypeId, CreatedAtUtc, CreateByUser, IsForeign, ExternalCode, Gender) Values
+(@FirstName, @LastName, @Address, @PartnerNumber, @CroatianPIN, @PartnerTypeId, CAST(GETUTCDATE() as date), @CreateByUser, @IsForeign, @ExternalCode, @Gender)";
+                        var result = db.Execute(sql, new
+                        {
+                            partner.FirstName,
+                            partner.LastName,
+                            partner.Address,
+                            partner.PartnerNumber,
+                            partner.CroatianPin,
+                            partner.PartnerTypeId,
+                            partner.CreateByUser,
+                            partner.IsForeign,
+                            partner.ExternalCode,
+                            partner.Gender
+                        });
+                        TempData["NewRow"] = partner.ExternalCode;
+                    }
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
---
+++ Models/Partner.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OsigDrustvo.Models
+{
+    public class Partner
+    {
+
+        public int PartnerId { get; set; }
+
+        [Required(ErrorMessage = "Required", AllowEmptyStrings = false)]
+        [Display(Name = "First name")]
+        [RegularExpression(@"^[a-zčćđšžA-ZČĆĐŠŽ0-9]*$", ErrorMessage = "Only alfanumeric characters")]
+        [StringLength(255, ErrorMessage = "Must be between 2 and 255 character lenght. Only alfanumeric characters", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Last name")]
+        [RegularExpression(@"^[a-zčćđšžA-ZČĆĐŠŽ0-9]*$", ErrorMessage = "Only alfanumeric characters")]
+        [StringLength(255, ErrorMessage = "Must be between 2 and 255 character lenght", MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "FullName")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+
+        [RegularExpression(@"^[a-zčćđšžA-ZČĆĐŠŽ0-9 ,]*$")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Partner number")]
+        [RegularExpression(@"^\d{20}$", ErrorMessage = "Must be exactly 20 numbers")]
+        public string PartnerNumber { get; set; }
+
+        [RegularExpression(@"^$|^[0-9]{11}$", ErrorMessage = "Croatian OIB must be 11 numbers")]
+        [Display(Name = "CroatianPIN")]
+        public string CroatianPin { get; set; }
+
+
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, 2, ErrorMessage = "Required")]
+        public int PartnerTypeId { get; set; }
+
+
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAtUtc { get; set; }
+
+        [Display(Name = "Created by user (email)")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        [StringLength(255, ErrorMessage = "Maximum lenght is 255 character")]
+        public string CreateByUser { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
+        public bool IsForeign { get; set; }
+
+
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[a-zA-Z0-9]*$")]
+        [Remote("IsExternalCodeAviable", "Default", ErrorMessage = "External code already exist")]
+        public string ExternalCode { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [UIHint("TemplateGender")]
+        public string Gender { get; set; }
+
+    }
+}
